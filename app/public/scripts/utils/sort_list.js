392bebf5a1539cde62e4fb3ff14ab02431b3a0e6
lukasunara <lukasunara7@gmail.com>
2021-06-08 20:30:29 +0200

    Fixed list sorting

--- app/public/scripts/utils/sort_list.js
+++ app/public/scripts/utils/sort_list.js
@@ -11,8 +11,8 @@ var sortListByDate = async (ascending) => {
     }
     // debugger;
     sortedArray.sort((el1, el2) => {
-        let el1Value = new Date(el1.dataset.date);
-        let el2Value = new Date(el2.dataset.date);
+        let el1Value = new Date(JSON.parse(el1.dataset.date));
+        let el2Value = new Date(JSON.parse(el2.dataset.date));
 
         let result = 0;
         if (el1Value < el2Value) result = -1;
@@ -21,17 +21,17 @@ var sortListByDate = async (ascending) => {
         return ascending ? result : -result;
     });
     // debugger;
-    for (let i = 0; i < elements.length; i++) {
-        list.replaceChild(sortedArray[i], elements[i]);
-        // elements[i].replaceWith(sortedArray[i]);
+    let newList = list.cloneNode(false);
+    for (let i = 0; i < sortedArray.length; i++) {
+        newList.appendChild(sortedArray[i]);
     }
-
-    // location.reload();
+    // debugger;
+    list.parentNode.replaceChild(newList, list);
 };
 
 // sort list by name
 var sortListByName = async (ascending) => {
-    // debugger;
+    debugger;
     let list = document.getElementById("listOfElements");
     let elements = list.getElementsByTagName("LI");
 
@@ -49,19 +49,24 @@ var sortListByName = async (ascending) => {
         return ascending ? result : -result;
     });
     // debugger;
-    for (let i = 0; i < elements.length; i++) {
-        list.replaceChild(sortedArray[i], elements[i]);
-        // elements[i].replaceWith(sortedArray[i]);
+    let newList = list.cloneNode(false);
+    for (let i = 0; i < sortedArray.length; i++) {
+        newList.appendChild(sortedArray[i]);
     }
-
-    // location.reload();
+    // debugger;
+    list.parentNode.replaceChild(newList, list);
 };
 
 // sort by name ascending
-document.getElementById("sortByNameAscBtn").addEventListener('click', sortListByName(true));
+document.getElementById("sortByNameAscBtn").addEventListener('click', () => sortListByName(true));
 // sort by name descending
-document.getElementById("sortByNameDescBtn").addEventListener('click', sortListByName(false));
+document.getElementById("sortByNameDescBtn").addEventListener('click', () => sortListByName(false));
+
 // sort by date ascending
-document.getElementById("sortByDateAscBtn").addEventListener('click', sortListByDate(true));
+let btnDateAsc = document.getElementById("sortByDateAscBtn");
+if (btnDateAsc)
+    btnDateAsc.addEventListener('click', () => sortListByDate(true));
 // sort by date descending
-document.getElementById("sortByDateDescBtn").addEventListener('click', sortListByDate(false));
+let btnDateDesc = document.getElementById("sortByDateDescBtn");
+if (btnDateDesc)
+    btnDateDesc.addEventListener('click', () => sortListByDate(false));
